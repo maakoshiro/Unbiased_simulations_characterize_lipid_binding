8567031eefbb1171da1a9e6b05204cb0de29f6a1
makoshiro <60958856+maakoshiro@users.noreply.github.com>
2024-11-01 10:45:41 +0900

    250v-workflow1.sh

--- Workflow/250v-workflow1.sh
+++ Workflow/250v-workflow1.sh
@@ -7,7 +7,7 @@ pdb=250v
 #Use CHARMM-GUI PDB Reader to prepare the PDB for the workflow
 #Untar the result, the workflow will use step1_pdbreader.pdb
 
-cp ./home/zy2024/protein/250v-step1_pdbreader.pdb
+cp ./protein/250v-step1_pdbreader.pdb
 
 #Changing the name of the histidines for martinize script
 sed -i 's/HSD/HIS/g' 250v-step1_pdbreader.pdb

0df82c0d8b593abef4f6ed6cbfdb6ea5f1dafe58
makoshiro <60958856+maakoshiro@users.noreply.github.com>
2024-11-08 12:55:25 +0900

    Create 250v-workflow1-1.sh

---
+++ Workflow/250v-workflow1-1.sh
@@ -0,0 +1,26 @@
+#Source the installed GROMACS in your workstation
+#source /opt/GROMACS/2021.2/bin/GMXRC
+
+#Write the name of the PDB ID used for the protocol
+pdb=250va
+
+#Use CHARMM-GUI PDB Reader to prepare the PDB for the workflow
+#Untar the result, the workflow will use step1_pdbreader.pdb
+
+cp ~/protein/250v-step1_pdbreader.pdb 250v-step1_pdbreader_his.pdb
+
+#Changing the name of the histidines for martinize script
+sed -i 's/HSD/HIS/g' 250v-step1_pdbreader_his.pdb
+sed -i '/HD1 HIS/d' 250v-step1_pdbreader_his.pdb
+
+#Make sure to change the path for DSSP
+martinize2 -f 250v-step1_pdbreader_his.pdb -x ${pdb}_cg_fc1000_eu8.pdb -o topol.top -ff martini3001 -elastic -ef 1000 -eu 0.8 -nt -scfix -cys auto -dssp ~/anaconda3/envs/py39/bin/dssp 
+
+#CHOOSE -merge <chainID> for multimers
+
+#Inserting the CG protein in a cubic box with 2 nm of distance
+gmx editconf -f ${pdb}_cg_fc1000_eu8.pdb  -c -d 2.0 -bt cubic -o ${pdb}_cg_newbox.gro
+
+cp ~/protein/common/cg.top .
+
+done

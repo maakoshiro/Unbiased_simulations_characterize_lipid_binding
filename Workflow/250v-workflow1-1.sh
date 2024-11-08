5d105f1c61e7b24c886cec680158737bc441d0aa
makoshiro <60958856+maakoshiro@users.noreply.github.com>
2024-11-08 13:44:37 +0900

    Update 250v-workflow1-1.sh

--- Workflow/250v-workflow1-1.sh
+++ Workflow/250v-workflow1-1.sh
@@ -14,14 +14,14 @@ sed -i 's/HSD/HIS/g' 250v-step1_pdbreader_his.pdb
 sed -i '/HD1 HIS/d' 250v-step1_pdbreader_his.pdb
 
 #Make sure to change the path for DSSP
-#martinize2 -f 250v-step1_pdbreader_his.pdb -x ${pdb}_cg_fc1000_eu8.pdb -o topol.top -ff martini3001 -elastic -ef 1000 -eu 0.8 -nt -scfix -cys auto -dssp ~/anaconda3/envs/py39/bin/dssp 
+martinize2 -f 250v-step1_pdbreader_his.pdb -x ${pdb}_cg_fc1000_eu8.pdb -o topol.top -ff martini3001 -elastic -ef 1000 -eu 0.8 -nt -scfix -cys auto -dssp ~/anaconda3/envs/py39/bin/dssp 
 
 #CHOOSE -merge <chainID> for multimers
 
 #Inserting the CG protein in a cubic box with 2 nm of distance
 #gmx editconf -f ${pdb}_cg_fc1000_eu8.pdb  -c -d 2.0 -bt cubic -o ${pdb}_cg_newbox.gro
 
-#cp ~/protein/common/cg.top .
+cp ~/protein/common/cg.top .
 
 #First minimization in vaccuum
 #gmx grompp -f ~/protein/common/min-vac.mdp -p cg.top -c ${pdb}_cg_newbox.gro -o min-vac.tpr
